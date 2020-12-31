65ecfed51770c2962dad7011e5001515ef467e98
Kelvin Do <kdo@plaid.com>
2020-12-30 18:06:34 -0800

    Decompose out tracklist

--- react-ui/src/Playlist.js
+++ react-ui/src/Playlist.js
@@ -1,15 +1,12 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react';
+import React, { useState } from 'react';
 import './Playlist.css';
+import Tracklist from './Tracklist';
  
 function Playlist() {
 
-  const audioRef = useRef();
   const [query, setQuery] = useState('37i9dQZF1DWSTc9FdySHtz');
   const [playlistNameOwner, setPlaylistNameOwner] = useState('')
   const [songSamples, setSongSamples] = useState(['', '']);
-  const [currentIndex, setCurrentIndex] = useState(1);
-  const [currentSongArtist, setCurrentSongArtist] = useState('');
-  const [currentSongSample, setCurrentSongSample] = useState('');
 
   //spotify:playlist:37i9dQZF1DWSTc9FdySHtz
   //spotify:playlist:5vwNi0Km340HsC5UfwaaIa
@@ -27,45 +24,10 @@ function Playlist() {
       .then(json => {
         setPlaylistNameOwner(json.playlist_name + " by " + json.playlist_owner);
         setSongSamples(json.song_datas);
-        setCurrentIndex(0);
       }).catch(e => {
         console.log(e);
       })
   };
-
-  const nextSong = useCallback(() => {
-    setCurrentIndex(prevIndex => (prevIndex + 1) % songSamples.length)
-  });
-
-  const handleUserKeyPress = useCallback(event => {
-    const { key } = event;
-
-    if (key === "ArrowRight") {
-      setCurrentIndex(prevIndex => (prevIndex + 1))
-    }
-
-    if (key === "ArrowLeft") {
-      setCurrentIndex(prevIndex => (prevIndex - 1))
-    }
-  }, []);
-
-  useEffect(() => {
-    const song = songSamples[currentIndex].name;
-    const artist = songSamples[currentIndex].artist;
-    const songArtist = song ? song + " by " + artist : "";
-    setCurrentSongArtist(songArtist);
-    setCurrentSongSample(songSamples[currentIndex].sample);
-    audioRef.current.load();
-  }, [currentIndex, playlistNameOwner]);
-
-   // Add event listeners
-   useEffect(() => {
-    window.addEventListener('keydown', handleUserKeyPress);
-    // Remove event listeners on cleanup
-    return () => {
-      window.removeEventListener('keydown', handleUserKeyPress);
-    };
-  }, []); // Empty array ensures that effect is only run on mount and unmount
 
   return (
     <div className="playlistMain">
@@ -78,13 +40,10 @@ function Playlist() {
         />
       </div>
       <button className="myButton" onClick={fetchSongSamples}>Load Playlist</button>
-      <button className="myButton" onClick={nextSong}>Next Song</button>
       <p>{ playlistNameOwner }</p>
-      <p>{currentIndex + 1}/{songSamples.length} {currentSongArtist}</p>
-      <audio controls autoPlay ref={audioRef}>
-        <source src={currentSongSample} type="audio/mp3"/>
-        Your browser does not support the audio element.
-      </audio>
+      <Tracklist
+        songSamples={songSamples}
+      />
     </div>
   );
 }
---
+++ react-ui/src/Tracklist.js
@@ -0,0 +1,63 @@
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import './Playlist.css';
+ 
+function Tracklist({ songSamples }) {
+  const audioRef = useRef();
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentSongArtist, setCurrentSongArtist] = useState('');
+  const [currentSongSample, setCurrentSongSample] = useState('');
+
+  const prevSong = useCallback(() => {
+    setCurrentIndex(prevIndex => (prevIndex - 1) % songSamples.length)
+  });
+
+  const nextSong = useCallback(() => {
+    setCurrentIndex(prevIndex => (prevIndex + 1) % songSamples.length)
+  });
+
+  const handleUserKeyPress = useCallback(event => {
+    const { key } = event;
+
+    if (key === "ArrowRight") {
+      setCurrentIndex(prevIndex => (prevIndex + 1))
+    }
+
+    if (key === "ArrowLeft") {
+      setCurrentIndex(prevIndex => (prevIndex - 1))
+    }
+  }, []);
+
+  useEffect(() => {
+    if (songSamples.length > 0) {
+      const song = songSamples[currentIndex].name;
+      const artist = songSamples[currentIndex].artist;
+      const songArtist = song ? song + " by " + artist : "";
+      setCurrentSongArtist(songArtist);
+      setCurrentSongSample(songSamples[currentIndex].sample);
+      audioRef.current.load();
+    }
+  }, [currentIndex]);
+
+   // Add event listeners
+   useEffect(() => {
+    window.addEventListener('keydown', handleUserKeyPress);
+    // Remove event listeners on cleanup
+    return () => {
+      window.removeEventListener('keydown', handleUserKeyPress);
+    };
+  }, []); // Empty array ensures that effect is only run on mount and unmount
+
+  return (
+    <div className="tracklistMain">
+      <button className="myButton" onClick={prevSong}>Prev</button>
+      <button className="myButton" onClick={nextSong}>Next</button>
+      <p>{currentIndex + 1}/{songSamples.length} {currentSongArtist}</p>
+      <audio controls autoPlay ref={audioRef}>
+        <source src={currentSongSample} type="audio/mp3"/>
+        Your browser does not support the audio element.
+      </audio>
+    </div>
+  );
+}
+ 
+export default Tracklist;
